d8c7211794a09cfce09528438a3e1ada85375b88
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-04-04 21:04:43 +0200

    Add registration form validation

--- src/pages/components-register/RegisterForm.js
+++ src/pages/components-register/RegisterForm.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link, Redirect } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
-import { Button, Form } from 'semantic-ui-react';
+import { Form, Input } from 'semantic-ui-react';
+
+import constraints from './constraints';
+import './errors.css';
 
 const LoginForm = () => {
+  const { handleSubmit, register, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <Form.Field>
         <label htmlFor="username">Username</label>
         <input
@@ -13,8 +23,14 @@ const LoginForm = () => {
           placeholder="Username"
           id="username"
           name="username"
+          ref={register(constraints.username)}
           required
         />
+        {errors.username && (
+          <p className="error">
+            Invalid username (must be 4-20 characters long)
+          </p>
+        )}
       </Form.Field>
       <Form.Field>
         <label htmlFor="email">E-mail</label>
@@ -23,8 +39,10 @@ const LoginForm = () => {
           placeholder="Email"
           id="email"
           name="email"
+          ref={register(constraints.email)}
           required
         />
+        {errors.email && <p className="error">Email is required</p>}
       </Form.Field>
       <Form.Field>
         <label htmlFor="password">Password</label>
@@ -33,8 +51,14 @@ const LoginForm = () => {
           placeholder="Password"
           id="password"
           name="password"
+          ref={register(constraints.password)}
           required
         />
+        {errors.password && (
+          <p className="error">
+            Invalid password (must be at least 5 characters long)
+          </p>
+        )}
       </Form.Field>
       <Form.Field>
         <label htmlFor="password2">Confirm password</label>
@@ -43,13 +67,18 @@ const LoginForm = () => {
           placeholder="Confirm password"
           id="password2"
           name="password2"
+          ref={register(constraints.password)}
           required
         />
+        {errors.password2 && (
+          <p className="error">
+            Invalid password (must be at least 5 characters long)
+          </p>
+        )}
       </Form.Field>
       {/* TODO: replace Link with a submit handler */}
-      <Link to="/dashboard">
-        <Button type="submit">Submit</Button>
-      </Link>
+
+      <Input type="submit" value="Submit" />
     </Form>
   );
 };
