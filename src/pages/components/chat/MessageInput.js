fd9df6fffc07d2a50a5ab07c0990f6359fe6191f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-04-07 16:12:10 +0200

    Handle submitting MessageInput.js

--- src/pages/components/chat/MessageInput.js
+++ src/pages/components/chat/MessageInput.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Form } from 'semantic-ui-react';
 
 const MessageInput = () => {
-  const onSubmit = (e) => {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log();
+
+    // TODO: replace with socket.emit()
+    console.log(message);
   };
 
   return (
-    <Form onSubmit={onSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Form.Group>
-        <Form.Input width={11} placeholder="Type in something..." />
+        <Form.Input
+          onChange={(e) => setMessage(e.target.value)}
+          width={11}
+          placeholder="Type in something..."
+        />
         <Form.Input type="submit" value="Send" />
       </Form.Group>
     </Form>
