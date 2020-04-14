f8d577482cbe8fd2ddc0967e865737f5e89f62ad
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-04-14 17:12:39 +0200

    Make log out work, redirect from log in page to the dashboard if a JWT is present

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 import { Container, Header } from 'semantic-ui-react';
 
 import LoginForm from './components/login/LoginForm';
 
 const Login = () => {
+  if (localStorage.getItem('jwt')) {
+    return <Redirect to="/dashboard" />;
+  }
+
   return (
     <Container className="animated fadeInUp" style={{ marginTop: '5vh' }}>
       <Header as="h1">Log in</Header>
